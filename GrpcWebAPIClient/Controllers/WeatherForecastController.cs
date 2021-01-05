6277d884da17cb84726c25cc13b3c7617f8cd87e
Awais Ilyas <im.awaisilyas@gmail.com>
2021-01-05 15:33:12 +0400

    WebAPI project added as client to test GRPC server

---
+++ GrpcWebAPIClient/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcWebAPIClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> GetAsync()
+        {
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var clientRequested = new CustomerLookupModel { UserId = 1 };
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
